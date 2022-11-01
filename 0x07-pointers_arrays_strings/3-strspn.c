843e31a07a14b14a387388d7ba16b1e3a1e5d0f7
paschal-wd <mosespaschal55@gmail.com>
2022-11-01 16:50:10 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,6 +3,7 @@
  * _strspn - search a string for a set of bytes
  * @s: source string
  * @accept: accepted string
+ *
  * Return: number of bytes in the init segment
  */
 unsigned int _strspn(char *s, char *accept)
