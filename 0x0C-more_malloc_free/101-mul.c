2f0c95f5e749958c08a2dfc722dc450d7593a825
paschal-wd <mosespaschal55@gmail.com>
2022-11-10 17:23:18 -0800

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -87,11 +87,11 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
+	printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-		printf("Error\n"), exit(98);
+	printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
 			{
 				add = (nout[k] - '0') + addl;
 				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
+				nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
 			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
