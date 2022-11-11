b0ca324128f0dcf16f24d2058c00ff00788377aa
paschal-wd <mosespaschal55@gmail.com>
2022-11-10 17:43:06 -0800

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -46,7 +46,7 @@ char *_initialize_array(char *ar, int lar)
 	int i = 0;
 
 	for (i = 0; i < lar; i++)
-	ar[i] = '0';
+		ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
@@ -64,11 +64,11 @@ int _checknum(char *argv[], int n)
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
-	if (!isdigit(argv[n][ln]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 
 	return (ln);
 }
@@ -87,11 +87,11 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-	printf("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-	printf("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
 			{
 				add = (nout[k] - '0') + addl;
 				if (add > 9)
-				nout[k - 1] = (add / 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
 			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
